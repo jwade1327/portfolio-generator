e252dbec924044fcc3995fa355ad7cb7a19493ff
unknown <jwade1327@gmail.com>
2020-07-23 00:15:18 -0500

    validation info

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const promptUser = () => {
     return inquirer.prompt([{
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "github",
-            message: "Enter your GitHub Username"
+            message: "Enter your GitHub Username (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
@@ -43,12 +59,28 @@ Add a New Project
     
             type: "input",
             name: "name",
-            message: "What is the name of your project?"
+            message: "What is the name of your project? (Required)",
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your project name!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "description",
-            message: "Provide a description of the project (Required)"
+            message: "Provide a description of the project (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of the project!");
+                    return false;
+                }
+            }
         },
         {
             type: "checkbox",
@@ -59,8 +91,15 @@ Add a New Project
         {
             type: "input",
             name: "link",
-            message: "Would you like to feature this project?",
-            default: false
+            message: "Would you like to feature this project? (Required)",
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your project GitHub link!");
+                    return false;
+                }
+            }
         },
         {
             type: "confirm",
